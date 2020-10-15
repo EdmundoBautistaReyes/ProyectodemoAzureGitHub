e43f0e359d26769fb195e9cd8f13705f106ec562
Edmundo Bautista Reyes <rtv.enlinea@hotmail.com>
2020-10-15 13:02:02 -0500

    Agregar nuevo proyecto web

---
+++ AppDemoAzureGitHub/AppDemoAzureGitHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppDemoAzureGitHub.Startup))]
+namespace AppDemoAzureGitHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
